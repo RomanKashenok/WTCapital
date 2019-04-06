86439b4da456def5e2d6d82bad16d51567389586
kashanok <r.kashanok@softteco.com>
2019-04-06 18:00:37 +0300

    commmit#11: Base functionality implemented

--- app/src/main/java/com/capitalquiz/db/DataLoader.kt
+++ app/src/main/java/com/capitalquiz/db/DataLoader.kt
@@ -1,25 +1,22 @@
 package com.capitalquiz.db
 
 import android.os.AsyncTask
+import com.capitalquiz.App
+import com.capitalquiz.R
 import com.capitalquiz.model.Country
 import com.capitalquiz.quiz.QuizDataHolder
 import com.capitalquiz.utils.url
-import com.capitalquiz.utils.url2
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 
 class DataLoader: AsyncTask<Unit, Unit, String>() {
 
     override fun doInBackground(vararg params: Unit?): String {
         return getDataFromUrl(url)
-    }
-
-    fun loadData() {
-        val countries = getDataFromUrl(url)
-        QuizDataHolder.countries = Gson().fromJson(countries, object : TypeToken<List<Country>>() {}.type)
     }
 
     private fun getDataFromUrl(url: String): String {
@@ -33,5 +30,28 @@ class DataLoader: AsyncTask<Unit, Unit, String>() {
 
     override fun onPostExecute(result: String) {
         QuizDataHolder.countries = ArrayList(Gson().fromJson(result, Array<Country>::class.java).toList())
+        if (QuizDataHolder.countries.size < 100) {
+            loadDataFromRaw()
+        }
+    }
+
+    private fun loadDataFromRaw() {
+        val context = App.instance.applicationContext
+        val filename = context.getString(R.string.capitals)
+        val resource =
+            context.resources.openRawResource(context.resources.getIdentifier(filename, "raw", context.packageName))
+        resource.use {
+            val reader = BufferedReader(InputStreamReader(resource))
+            var line: String? = reader.readLine()
+            var index = 1
+            while (line != null) {
+                val keyValuePair = line.split("--".toRegex())
+                val country = Country(index, keyValuePair[0], keyValuePair[1])
+                index++
+                line = reader.readLine()
+                QuizDataHolder.countries.add(country)
+            }
+            resource.close()
+        }
     }
 }
