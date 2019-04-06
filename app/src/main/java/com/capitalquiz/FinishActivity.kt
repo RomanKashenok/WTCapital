752d38b409bad76db3fdc5ee242630a849001dbc
user <usermail@gmail.com>
2019-04-06 15:26:02 +0300

    commit#4 ads implementation

--- app/src/main/java/com/capitalquiz/FinishActivity.kt
+++ app/src/main/java/com/capitalquiz/FinishActivity.kt
@@ -1,6 +1,42 @@
 package com.capitalquiz
 
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.ads.MobileAds
+import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
-class FinishActivity: AppCompatActivity() {
+
+class FinishActivity : AppCompatActivity() {
+
+    lateinit var mAdView: AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        MobileAds.initialize(this, "ca-app-pub-1867687838298230~9902359578")
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
+
+    public override fun onPause() {
+        mAdView.pause()
+        super.onPause()
+    }
+
+
+    public override fun onResume() {
+        super.onResume()
+        mAdView.resume()
+    }
+
+
+    public override fun onDestroy() {
+        mAdView.destroy()
+        super.onDestroy()
+    }
 }
